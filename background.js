33c5a96ece6883870010fd3452fac204cfd8e171
Matt757 <matei@capan.ro>
2023-12-02 20:48:39 +0200

    Check for undefined changeInfo.url value

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-        // read changeInfo data and do something with it (like read the url)
-        if (changeInfo.url.includes("youtube.com/shorts")) {
-            console.log(changeInfo);
-            chrome.tabs.update(tabId, { url: changeInfo.url.replace("shorts", "watch") });
-        }
+  if (changeInfo.url) {
+    if (changeInfo.url.includes("youtube.com/shorts")) {
+      chrome.tabs.update(tabId, {
+        url: changeInfo.url.replace("shorts", "watch"),
+      });
     }
-);
+  }
+});
